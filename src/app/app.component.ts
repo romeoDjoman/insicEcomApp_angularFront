888d9201a47efc416fcd4ad3bd6b6cd0ddd1e402
romeoDjoman <romeo.djoman@outlook.fr>
2025-01-21 20:59:44 +0100

    Set routing for pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './features/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',
+        component: HomeComponent
+    },
+    { 
+        path: 'products', 
+        loadComponent: () => 
+            import('./features/products/product-list/product-list.component')
+            .then(m => m.ProductListComponent) 
+    },
+    { 
+        path: 'product/:id', 
+        loadComponent: () => 
+            import('./features/products/product-detail/product-detail.component')
+            .then(m => m.ProductDetailComponent) 
+    },
+    { 
+        path: 'cart', 
+        loadComponent: () => 
+            import('./features/cart/cart.component')
+            .then(m => m.CartComponent) 
+    },
+    { 
+        path: 'checkout', 
+        loadComponent: () => 
+            import('./features/checkout/checkout.component')
+            .then(m => m.CheckoutComponent) 
+    },
+    { 
+        path: 'auth/login', 
+        loadComponent: () => 
+            import('./features/auth/login/login.component')
+            .then(m => m.LoginComponent) 
+    },
+    { 
+        path: 'auth/register', 
+        loadComponent: () => 
+            import('./features/auth/register/register.component')
+            .then(m => m.RegisterComponent) 
+    },
+    { 
+        path: '**', 
+        redirectTo: '' 
+    }
+];
